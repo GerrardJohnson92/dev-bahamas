ba1bc9a5e1e0ff7cd980cd9fc2e430d92d9a818b
Gerrard Johnson <38636463+GerrardJohnson92@users.noreply.github.com>
2020-03-18 13:58:14 -0500

    Update js_app.js

--- js/js_app.js
+++ js/js_app.js
@@ -19,7 +19,7 @@ const showCoffees = () => {
                   <div class="card">
                     <img class="card--avatar" src=${image} />
                     <h1 class="card--title">${name}</h1>
-                    <a class="card--link" href="https://www.google.com/">More</a>
+                    <a class="card--link" href="./beaches.html">More</a>
                   </div>
                 `)
     )
