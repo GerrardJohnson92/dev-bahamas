ebea6ada1da93be90458d3fbb942a8443ec62089
Gerrard Johnson <38636463+GerrardJohnson92@users.noreply.github.com>
2020-03-18 11:16:12 -0500

    Update js_app.js

--- js/js_app.js
+++ js/js_app.js
@@ -30,7 +30,7 @@ const showCoffees = () => {
 
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('../sw.js').then( () => {
+      navigator.serviceWorker.register('./sw.js').then( () => {
         console.log('Service Worker Registered')
       })
     })
